54904f950ce9b80a61577d33e3279bc05c9a495f
root <root@localhost.localdomain>
2020-11-02 03:18:47 +0300

    Initial

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+ 	"log"
+	"io/ioutil"
+	"github.com/Belyakoff/product-api/handlers"
+) 
+
+
+func main(){
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
+			
+	})
+
+
+	http.ListenAndServe(":9090", nil)
+
+}
