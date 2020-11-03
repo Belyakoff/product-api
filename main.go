49e82e9f763086ab6b5b089e35eb31e4c3bf1e0c
root <root@localhost.localdomain>
2020-11-03 21:11:40 +0300

    added goodbye handler and graceful shutdown

--- main.go
+++ main.go
@@ -1,26 +1,52 @@
 package main
 
 import (
-	"net/http"
-	"os"
- 	"log"
 	"github.com/Belyakoff/product-api/handlers"
+	"net/http"
+	"os" 
+	"os/signal"
+ 	"log"
+ 	"time"
+ 	"context"	
 ) 
 
 
 func main(){
 
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l  := log.New(os.Stdout, "product-api", log.LstdFlags)
+	
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
-			
-	})
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120*time.Second,
+		ReadTimeout: 1*time.Second,
+		WriteTimeout: 1*time.Second,
+	}
 
+	go func(){
+		l.Println("Starting server on port 9090")
 
-	http.ListenAndServe(":9090", nil)
+		err := s.ListenAndServe()
+		if err != nil {
+		 		l.Fatal(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	sig := <-c
+	log.Println("Received terminate, graceful shutdown:", sig)
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 
 }
